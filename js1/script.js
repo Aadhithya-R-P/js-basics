8c5eefe0cb59298ccc1c6abfd4a4979d88486eb6
Aadhithya R P <96223165+Aadhithya-R-P@users.noreply.github.com>
2023-04-03 15:19:49 +0530

    Add files via upload

---
+++ js1/script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button')
+const body = document.querySelector("body")
+
+button.addEventListener('click', button=> {
+    body.style.backgroundColor = 'yellow';
+})
